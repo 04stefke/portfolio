109bc402c61402f997d989ae31249a62fbe60200
04stefke <jovanovics399@yahoo.com>
2024-09-04 19:27:15 +0100

    half styled the left component

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import {
+	faFacebookF,
+	faGithub,
+	faInstagram,
+	faLinkedinIn,
+	faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
-}
+	return (
+		<div className=" h-full flex flex-col justify-around my-10">
+			<div className="flex flex-col justify-around h-1/4 ">
+				<div className="">
+					<h1 className="text-5xl font-bold">Stefan Jovanovic</h1>
+					<h3 className="text-2xl font-bold">Jr Frontend Developer</h3>
+				</div>
+				<div>
+					<p className="uppercase">ABOUT</p>
+					<p className="uppercase">Experiences</p>
+					<p className="uppercase">Projects</p>
+				</div>
+			</div>
+			<div className="flex gap-2 items-center justify-start text-2xl only:justify-end">
+				<a href="https://github.com/04stefke">
+					<FontAwesomeIcon icon={faGithub} />
+				</a>
+				<a href="https://www.linkedin.com/in/stefan-jovanovic-73081929a/">
+					<FontAwesomeIcon icon={faLinkedinIn} />
+				</a>
+				<a href="">
+					<FontAwesomeIcon icon={faInstagram} />
+				</a>
+				<a href="">
+					<FontAwesomeIcon icon={faFacebookF} />
+				</a>
+				<a href="">
+					<FontAwesomeIcon icon={faXTwitter} />
+				</a>
+			</div>
+		</div>
+	);
+};
 
-export default Intro
+export default Intro;
