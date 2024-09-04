175e91ff495fd819b4912ecb43b684662beb966d
04stefke <jovanovics399@yahoo.com>
2024-09-04 18:37:12 +0100

    created main page and positioned it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Intro from "./components/Intro/Intro";
 
 function App() {
-	return <></>;
+	return (
+		<div className="w-full h-[100dvh] flex items-center justify-center">
+			<Intro />
+		</div>
+	);
 }
 
 export default App;
