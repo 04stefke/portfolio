dac390b50a7c387e4c56540e120805fb7525d004
04stefke <jovanovics399@yahoo.com>
2024-09-05 09:40:41 +0100

    added files/folders

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,7 +6,7 @@ import {
 	faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+
 
 const Intro = () => {
 	return (
