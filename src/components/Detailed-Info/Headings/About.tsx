6ea2ae99de947f736cd3ca7d13e0df651dba5302
04stefke <jovanovics399@yahoo.com>
2024-09-08 20:11:49 +0100

    added the about section and styled it

--- src/components/Detailed-Info/Headings/About.tsx
+++ src/components/Detailed-Info/Headings/About.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<div className="flex flex-col gap-5">
+			<h1 className="text-3xl font-bold">About</h1>
+			<p className="text-lg">
+				Dedicated 20-year-old engineer passionate about translating designs into
+				dynamic user interfaces. With a robust skill set in HTML5, CSS3, SASS,
+				Material UI, JavaScript, ReactJS, and Redux, I excel in developing
+				sophisticated, interactive web applications. Through various projects,
+				including a Reddit clone and an e-commerce site, I have demonstrated my
+				ability to deliver high-quality, responsive designs. Committed to
+				continuous improvement and effective teamwork, I am eager to further my
+				career in front-end development.
+			</p>
+		</div>
+	);
+};
 
-export default About
+export default About;
