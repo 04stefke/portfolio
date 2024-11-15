8c89c3b507414fbd4a3f572ae97c9266f689f913
04stefke <jovanovics399@yahoo.com>
2024-11-15 13:02:21 +0000

    added the project structure and styled it

--- src/components/Detailed-Info/Headings/Project.tsx
+++ src/components/Detailed-Info/Headings/Project.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React from "react";
+import redditImg from "../../../images/RedditApp.png";
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
-}
+	return (
+		<div className="flex flex-col gap-10 w-full">
+			<a href="">
+				<div className="flex flex-row gap-10 rounded hover:bg-hoverBg py-3 px-2">
+					<img src={redditImg} alt="" className="w-24 rounded-xl" />
+					<div className="flex flex-col gap-2">
+						<h2 className="text-xl font-bold text-fontColor">Project Title</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+							dolores adipisci sed! Praesentium vero voluptatem deserunt
+							pariatur. Accusamus tenetur distinctio vel perspiciatis magnam
+							similique. Eos, voluptas? Inventore ducimus illum officia?
+						</p>
+					</div>
+				</div>
+			</a>
+			<a href="">
+				<div className="flex flex-row gap-10 rounded hover:bg-hoverBg py-3 px-2">
+					<img src={redditImg} alt="" className="w-24 rounded-xl" />
+					<div className="flex flex-col gap-2">
+						<h2 className="text-xl font-bold text-fontColor">Project Title</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+							dolores adipisci sed! Praesentium vero voluptatem deserunt
+							pariatur. Accusamus tenetur distinctio vel perspiciatis magnam
+							similique. Eos, voluptas? Inventore ducimus illum officia?
+						</p>
+					</div>
+				</div>
+			</a>
+			<a href="">
+				<div className="flex flex-row gap-10 rounded hover:bg-hoverBg py-3 px-2">
+					<img src={redditImg} alt="" className="w-24 rounded-xl" />
+					<div className="flex flex-col gap-2">
+						<h2 className="text-xl font-bold text-fontColor">Project Title</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+							dolores adipisci sed! Praesentium vero voluptatem deserunt
+							pariatur. Accusamus tenetur distinctio vel perspiciatis magnam
+							similique. Eos, voluptas? Inventore ducimus illum officia?
+						</p>
+					</div>
+				</div>
+			</a>
+		</div>
+	);
+};
 
-export default Project
+export default Project;
