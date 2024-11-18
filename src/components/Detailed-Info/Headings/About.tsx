7dfc8a00dfc8c10e893a9b2f096a65a6c93775e3
04stefke <jovanovics399@yahoo.com>
2024-11-18 17:35:29 +0000

    updated the about me paragraphs  and bolded key words

--- src/components/Detailed-Info/Headings/About.tsx
+++ src/components/Detailed-Info/Headings/About.tsx
@@ -1,18 +1,32 @@
 const About = () => {
 	return (
-		<section id="about" >
+		<section id="about">
 			<p className="text-sm">
-				Dedicated 20-year-old engineer passionate about translating designs into
-				dynamic user interfaces. With a robust skill set in
-				<strong> HTML5</strong>, <strong>CSS3</strong>,<strong> SASS</strong>,{" "}
-				<strong>Material UI</strong>,<strong> JavaScript </strong>,{" "}
-				<strong>ReactJS </strong>, and
-				<strong> Redux</strong>, I excel in developing sophisticated,
-				interactive web applications. Through various projects, including a
-				<strong> Reddit clone</strong> and an <strong>e-commerce site</strong>,
-				I have demonstrated my ability to deliver high-quality, responsive
-				designs. Committed to continuous improvement and effective teamwork, I
-				am eager to further my career in <strong>front-end development</strong>.
+				I’m a 20-year-old front-end engineer passionate about creating seamless,
+				dynamic user interfaces that combine elegant design with robust
+				functionality. With a solid foundation in modern web development
+				technologies—including <strong>HTML5</strong>, <strong>CSS3</strong>,{" "}
+				<strong>SASS</strong>, <strong>Material UI</strong>,{" "}
+				<strong>JavaScript</strong>, <strong>ReactJS</strong>, and{" "}
+				<strong>Redux</strong>. I specialize in building interactive, responsive
+				web applications that offer exceptional user experiences. <br /> <br />{" "}
+				My portfolio includes projects like a <strong>Reddit clone</strong> and
+				a fully functional <strong>e-commerce platform</strong>, where I
+				demonstrated my ability to craft scalable, feature-rich applications
+				while adhering to best practices in web development. I take pride in
+				translating complex designs into pixel-perfect implementations and
+				ensuring usability across devices and browsers. <br /> <br /> Beyond my
+				<strong> technical skills</strong>, I thrive in{" "}
+				<strong>collaborative environments</strong>,{" "}
+				<strong>valuing clear communication</strong> and{" "}
+				<strong>teamwork</strong> to deliver results efficiently. I'm deeply
+				committed to continuous learning, staying up-to-date with the latest
+				advancements in front-end technologies, and refining my craft to tackle
+				increasingly complex challenges. <br /> <br /> In my free time, you’ll
+				often find me in the gym, diving into code challenges to sharpen my
+				problem-solving skills. As I continue to grow in my career, I’m excited
+				to contribute to innovative projects and make an impact by bridging
+				design and technology in meaningful ways.
 			</p>
 		</section>
 	);
