e11d541bda70ae97da3df0cee62628ff04fd25d3
04stefke <jovanovics399@yahoo.com>
2024-11-26 19:47:39 +0000

    added another challenge and moved the previous to the top

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -31,6 +31,70 @@ const Projects = () => {
 							<td className="py-4 pr-4 align-top text-xs">2024</td>
 							<td className="py-4 pr-4 align-top text-fontColor">
 								<a
+									href="https://product-list-with-cart-challenge.netlify.app/"
+									target="_blank"
+								>
+									Product-List-With-Cart-Challenge
+								</a>
+							</td>
+							<td className="py-4 pr-4 align-top hidden sm:table-cell">
+								<div className="flex  items-center gap-1 ">
+									<p className="bubbles">Html</p>
+									<p className="bubbles">CSS</p>
+									<p className="bubbles">Tailwind</p>
+									<p className="bubbles">React</p>
+									<p className="bubbles">React-Use-Cart</p>
+									<p className="bubbles">GitHub</p>
+									<p className="bubbles">Netlify</p>
+								</div>
+							</td>
+							<td className="hidden md:table-cell py-4 pr-4">
+								<a
+									href="https://product-list-with-cart-challenge.netlify.app/"
+									target="_blank"
+									className="hover:text-fontColor"
+								>
+									<FontAwesomeIcon
+										icon={faArrowUpRightFromSquare}
+									></FontAwesomeIcon>
+								</a>
+							</td>
+						</tr>
+						<tr className="border-b">
+							<td className="py-4 pr-4 align-top text-xs">2024</td>
+							<td className="py-4 pr-4 align-top text-fontColor">
+								<a
+									href="https://bento-grid-challange.netlify.app/"
+									target="_blank"
+								>
+									Bento Grid Challenge
+								</a>
+							</td>
+							<td className="py-4 pr-4 align-top hidden sm:table-cell">
+								<div className="flex  items-center gap-1 ">
+									<p className="bubbles">Html</p>
+									<p className="bubbles">CSS</p>
+									<p className="bubbles">Tailwind</p>
+									<p className="bubbles">GitHub</p>
+									<p className="bubbles">Netlify</p>
+								</div>
+							</td>
+							<td className="hidden md:table-cell py-4 pr-4">
+								<a
+									href="https://bento-grid-challange.netlify.app/"
+									target="_blank"
+									className="hover:text-fontColor"
+								>
+									<FontAwesomeIcon
+										icon={faArrowUpRightFromSquare}
+									></FontAwesomeIcon>
+								</a>
+							</td>
+						</tr>
+						<tr className="border-b">
+							<td className="py-4 pr-4 align-top text-xs">2024</td>
+							<td className="py-4 pr-4 align-top text-fontColor">
+								<a
 									href="https://stefkes-youtube2mp3.netlify.app"
 									target="_blank"
 								>
@@ -113,37 +177,6 @@ const Projects = () => {
 							<td className="hidden md:table-cell py-4 pr-4">
 								<a
 									href="https://stefans-javascript-quiz.netlify.app"
-									target="_blank"
-									className="hover:text-fontColor"
-								>
-									<FontAwesomeIcon
-										icon={faArrowUpRightFromSquare}
-									></FontAwesomeIcon>
-								</a>
-							</td>
-						</tr>
-						<tr className="border-b">
-							<td className="py-4 pr-4 align-top text-xs">2024</td>
-							<td className="py-4 pr-4 align-top text-fontColor">
-								<a
-									href="https://bento-grid-challange.netlify.app/"
-									target="_blank"
-								>
-									Bento Grid Challenge
-								</a>
-							</td>
-							<td className="py-4 pr-4 align-top hidden sm:table-cell">
-								<div className="flex  items-center gap-1 ">
-									<p className="bubbles">Html</p>
-									<p className="bubbles">CSS</p>
-									<p className="bubbles">Tailwind</p>
-									<p className="bubbles">GitHub</p>
-									<p className="bubbles">Netlify</p>
-								</div>
-							</td>
-							<td className="hidden md:table-cell py-4 pr-4">
-								<a
-									href="https://bento-grid-challange.netlify.app/"
 									target="_blank"
 									className="hover:text-fontColor"
 								>
